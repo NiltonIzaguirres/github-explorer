98aa414e0736aa19c00809553df51c03e207a6c3
Nilton Izaguirres <niltonizaguirres@gmail.com>
2022-08-30 12:58:22 -0300

    feat: added search repositories by username

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -1,7 +1,7 @@
-import { useEffect, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import { RepositoryItem } from "./RepositoryItem";
 
-import '../styles/repositories.scss';
+import "../styles/repositories.scss";
 
 interface Repository {
   name: string;
@@ -11,22 +11,54 @@ interface Repository {
 
 export function RepositoryList() {
   const [repositories, setRepositories] = useState<Repository[]>([]);
+  const [repositoryName, setRepositoryName] = useState("");
 
-  useEffect(()=>{
-    fetch('https://api.github.com/orgs/rocketseat/repos')
-      .then(response => response.json())
-      .then(data => setRepositories(data));
-  }, [])
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    if (repositoryName.length !== 0) {
+      try {
+        fetch(`https://api.github.com/users/${repositoryName}/repos`)
+          .then((response) => response.json())
+          .then((data) =>
+            data.message === "Not Found"
+              ? setRepositories([])
+              : setRepositories(data)
+          );
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  }
+
+  useEffect(() => {
+    fetch(`https://api.github.com/users/niltonizaguirres/repos`)
+      .then((response) => response.json())
+      .then((data) => setRepositories(data));
+  }, []);
 
   return (
-    <section className="repository-list">
+    <div className="repository-list">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Digite o nome do usuário"
+          value={repositoryName}
+          onChange={(e) => setRepositoryName(e.target.value)}
+        />
+        <button type="submit">Buscar</button>
+      </form>
       <h1>📚 Lista de repositórios</h1>
 
       <ul>
-        {repositories.map(repository =>
-          <RepositoryItem key={repository.name} repository={repository} />
+        {repositories.length === 0 ? (
+          <h2>Nenhum repositório encontrado</h2>
+        ) : (
+          repositories.map((repository) => (
+            <RepositoryItem key={repository.name} repository={repository} />
+          ))
         )}
       </ul>
-    </section>
-  )
+    </div>
+  );
 }
